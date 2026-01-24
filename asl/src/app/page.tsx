58e18b29efd167f7f4ea39660a816975395854c2
Eric George <ericgeorge@endpoint-10-192-49-8.ufvisitor.ufl.edu>
2026-01-24 02:40:36 -0500

    elevenlabs boilerplate

--- asl/src/app/page.tsx
+++ asl/src/app/page.tsx
@@ -1,21 +1,17 @@
-import Link from "next/link";
-
-import {Navbar} from "@/app/_components/navbar";
-
+import { Navbar } from "@/app/_components/navbar";
 import { api, HydrateClient } from "~/trpc/server";
+import { ElevenLabsDemo } from "@/app/_components/eleven-labs-demo";
 
 export default async function Home() {
-  return (
-    <div className="">
-      <HydrateClient>
-      <Navbar></Navbar>
+    return (
+        <div className="">
+            <HydrateClient>
+                <Navbar></Navbar>
 
-
-
-      </HydrateClient>
-
-      
-    </div>
-    
-  );
+                <main className="flex flex-col items-center justify-center p-8">
+                    <ElevenLabsDemo />
+                </main>
+            </HydrateClient>
+        </div>
+    );
 }
